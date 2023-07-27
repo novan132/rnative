6b7789dac5a606d9f2b3cc5930c05e2f32721795
Novan <l4novan132@gmail.com>
2023-07-27 17:39:22 +0700

    creat like button

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,28 @@
-import { View, Text } from 'react-native';
-export const CircleButton = () => {
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { COLORS, SIZES, FONTS, SHADOWS } from '../constants';
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        borderRadius: SIZES.extraLarge,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMethod="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
   );
 };
 
