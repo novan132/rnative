eb4aaffdb5339b063ad5decf00612cdcbdb01a26
Novan <l4novan132@gmail.com>
2023-07-27 17:33:29 +0700

    feat: add button

---
+++ components/Button.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+export const CircleButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
