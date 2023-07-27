641107ef2364d53faa09fa2175cfe2475c465e63
Novan <l4novan132@gmail.com>
2023-07-27 13:38:46 +0700

    feat: create component for the screens

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const FocusedStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusedStatusBar</Text>
+    </View>
+  );
+};
+export default FocusedStatusBar;
---
+++ components/NFTCard.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const NFTCard = () => {
+  return (
+    <View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+};
+export default NFTCard;
