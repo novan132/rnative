3bc5e915b5583102318e7d8c43ada1ed3235ce35
Novan <l4novan132@gmail.com>
2023-07-27 17:10:12 +0700

    feat: add list of data to home

--- screens/Home.js
+++ screens/Home.js
@@ -8,6 +8,14 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
