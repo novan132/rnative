67e1f8b3ae90bfd360793853ce91a6dc92fbd4b0
Novan <l4novan132@gmail.com>
2023-07-28 08:21:06 +0700

    feat: implement bid button

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -58,7 +58,11 @@ const NFTCard = ({ data }) => {
           }}
         >
           <EthPrice price={data.price} />
-          <RectButton />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate('Details', { data })}
+          />
         </View>
       </View>
     </View>
